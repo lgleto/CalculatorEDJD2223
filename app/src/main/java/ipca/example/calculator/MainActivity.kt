f45229db1ea8959b244ced1e09d5dba51338e8b7
lourencogomes <leto.lg@gmail.com>
2022-10-11 14:33:31 +0100

    - operation

--- app/src/main/java/ipca/example/calculator/MainActivity.kt
+++ app/src/main/java/ipca/example/calculator/MainActivity.kt
@@ -6,18 +6,55 @@ import android.view.View
 import android.widget.Button
 import ipca.example.calculator.databinding.ActivityMainBinding
 
+enum class Operation(val op : String) {
+    ADD("+") ,
+    SUBTRACT("-"),
+    MULTIPLY("*"),
+    DIVISION("/")
+}
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
 
+    var operand = 0.0
+    var operation : Operation = Operation.ADD
+
+    private var onOperationPressed : ((View) -> Unit)? = {
+        val buttonPressed = it as? Button
+        operation = when (buttonPressed?.text){
+            "+" -> Operation.ADD
+            "-" -> Operation.SUBTRACT
+            "*" -> Operation.MULTIPLY
+            "/" -> Operation.DIVISION
+            else -> Operation.ADD
+        }
+        operand = binding.textViewDisplay.text.toString().toDouble()
+    }
+
     private var onNumberPressed : ((View) -> Unit)? = {
         val buttonPressed = it as? Button
-        binding.textViewDisplay.text = binding.textViewDisplay.text.toString() + buttonPressed?.text
+        val buttonText : String = buttonPressed?.text.toString()
+        val displayText : String = binding.textViewDisplay.text.toString()
+        if (displayText == "0" ){
+            if (buttonText == "."){
+                binding.textViewDisplay.text = displayText + buttonText
+            }else{
+                binding.textViewDisplay.text = buttonText
+            }
+        }else{
+            if (buttonText == "."){
+                if (!displayText.contains(".")){
+                    binding.textViewDisplay.text = displayText + buttonText
+                }
+            }else{
+                binding.textViewDisplay.text = displayText + buttonText
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -32,6 +69,13 @@ class MainActivity : AppCompatActivity() {
         binding.button8.setOnClickListener(onNumberPressed)
         binding.button9.setOnClickListener(onNumberPressed)
         binding.button0.setOnClickListener(onNumberPressed)
+        binding.buttonDot.setOnClickListener(onNumberPressed)
+
+        binding.buttonPlus.setOnClickListener(onOperationPressed)
+        binding.buttonDivide.setOnClickListener(onOperationPressed)
+        binding.buttonMinus.setOnClickListener(onOperationPressed)
+        binding.buttonMultiply.setOnClickListener(onOperationPressed)
+
     }
 
 }
